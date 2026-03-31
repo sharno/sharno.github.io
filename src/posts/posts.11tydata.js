c46efcaf4a12a4ff0b8bee69c6fb704ae4847864
sharno <sharnoby3@gmail.com>
2026-03-30 23:28:06 -0400

    Auto-detect RTL for Arabic posts

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,9 +1,21 @@
 const { DateTime } = require("luxon");
 
+function looksArabic(value) {
+  return /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF]/.test(value || "");
+}
+
 module.exports = {
   layout: "post.njk",
   tags: ["posts"],
   eleventyComputed: {
+    lang: (data) => {
+      if (data.lang) return data.lang;
+      return looksArabic(data.title) ? "ar" : "en-us";
+    },
+    dir: (data) => {
+      if (data.dir) return data.dir;
+      return looksArabic(data.title) ? "rtl" : "ltr";
+    },
     permalink: (data) => {
       const published = DateTime.fromJSDate(data.page.date, { zone: "utc" });
       return `post/${published.toFormat("yyyy/LL/dd")}/${data.page.fileSlug}/index.html`;
